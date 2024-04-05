928e83c5e620e0d8da708a6964b1a237943965cc
Lotar <102708361+Lotar17@users.noreply.github.com>
2024-04-05 17:24:25 -0300

    Agregar mas parametros en la entrada de la consola

--- TP1 Ruleta.py
+++ TP1 Ruleta.py
@@ -1,15 +1,22 @@
 import random
 import sys
+import matplotlib.pyplot as plt
 
-# Verificar si se proporciona el número de valores como argumento
-if len(sys.argv) != 3 or sys.argv[1] != "-n":
-    print("Uso: python programa.py -n <num_valores>")
-    sys.exit(1)
 
+def are_arguments_ok():
+    # Verificar si los argumentos enviados son correctos
+    if len(sys.argv) != 7 or sys.argv[1] != "-c" or sys.argv[3] !='-n' or sys.argv[5]!='-e':
+        print("Uso: python programa.py -c <cantidad de tiradas> -n <cantidad de corridas> -e <numero elegido> ")
+        sys.exit(1)
+
+
+are_arguments_ok()
 # Obtener el número de valores de los argumentos de la línea de comandos
 num_valores = int(sys.argv[2])
+corridas = int(sys.argv[4])
+numero_elegido = int(sys.argv[6])
 
-# Generar los valores aleatorios entre 0 y 1 y almacenarlos en una lista
+# Generar los valores aleatorios entre 0 y 36 y almacenarlos en una lista
 valores = [random.randint(0, 37) for _ in range(num_valores)]
 
 # Calcular la frecuencia absoluta de cada valor
@@ -20,9 +27,24 @@ frecuencia_absoluta = {i: valores.count(i) for i in range(0,37)}
 # Calcular la frecuencia relativa de cada valor
 frecuencia_relativa  = {i: frecuencia_absoluta[i] / num_valores for i in range(0,37)}
 
+y1= list(range(10))
+x1= list(range(num_valores))
+
+# Crear el gráfico
+plt.figure(figsize=(10, 6))
+plt.plot(x1, y1, color='red')
+plt.xlabel('Número de tirada')
+plt.ylabel('Frecuencia relativa')
+plt.title('Frecuencia relativa')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
 # Imprimir los resultados
 print("Valores generados:", valores)
 for i in range(0,37):
     print(f"Frecuencia absoluta de {i}:", frecuencia_absoluta[i])
     print(f"Frecuencia relativa de {i}:", frecuencia_relativa[i])
 
+print(f'Numeros de corridas {corridas}, Numero elegido: {numero_elegido}')
