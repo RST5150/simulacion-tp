fc9f5dafc0fc135d6adddc1b0a263411b6695d30
unknown <tomasperesin@gmail.com>
2024-05-05 17:27:59 -0300

    cuerpo base y martingala

--- FuncionesRuleta.py
+++ FuncionesRuleta.py
@@ -6,39 +6,86 @@ import math
 
 NUMEROS_RULETA = 37
 VALORES_BASES = list(range(NUMEROS_RULETA))
+ROJOS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+NEGROS = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
+FILAS = {
+    1: [1, 2, 3],
+    2: [4, 5, 6],
+    3: [7, 8, 9],
+    4: [10, 11, 12],
+    5: [13, 14, 15],
+    6: [16, 17, 18],
+    7: [19, 20, 21],
+    8: [22, 23, 24],
+    9: [25, 26, 27],
+    10: [28, 29, 30],
+    11: [31, 32, 33],
+    12: [34, 35, 36]
+}
+
+COLUMNAS = {
+    1: [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
+    2: [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+    3: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+}
 
 
 def are_arguments_ok():
-    # Verificar si los argumentos enviados son correctos
-    if len(sys.argv) != 7 or sys.argv[1] != "-c" or sys.argv[3] != '-n' or sys.argv[5] != '-e':
-        print("Uso: python programa.py -c <cantidad de tiradas> -n <cantidad de corridas> -e <numero elegido> ")
+    if len(sys.argv) != 7 and len(sys.argv) != 9:
+        print("Uso: python programa.py -c <cantidad de tiradas> -n <cantidad de corridas> [-e <numero elegido>] -s <estrategia> -a <tipo de capital>")
         sys.exit(1)
 
-    # Verificar si el valor después de "-e" está dentro del rango 0-36
-    try:
-        numero_elegido = int(sys.argv[6])
-        if numero_elegido < 0 or numero_elegido > 36:
-            print("El número elegido debe estar entre 0 y 36.")
+    if '-e' in sys.argv:
+        try:
+            indice = sys.argv.index('-e')
+            numero_elegido = int(sys.argv[indice + 1])
+            if numero_elegido < 0 or numero_elegido > 36:
+                print("El número elegido debe estar entre 0 y 36.")
+                sys.exit(1)
+        except ValueError:
+            print("El valor después de '-e' debe ser un número entero.")
             sys.exit(1)
-    except ValueError:
-        print("El valor después de '-e' debe ser un número entero.")
+        except IndexError:
+            print("Falta el valor después de '-e'.")
+            sys.exit(1)
+
+    if '-s' not in sys.argv or '-a' not in sys.argv:
+        print("Falta la estrategia (-s) o el tipo de capital (-a).")
         sys.exit(1)
 
+    estrategia = sys.argv[sys.argv.index('-s') + 1]
+    if estrategia not in ['m', 'd', 'f']:
+        print("La estrategia debe ser 'm' (Martingala), 'd' (D’Alambert), o 'f' (Fibonacci).")
+        sys.exit(1)
 
-are_arguments_ok()
+    capital = sys.argv[sys.argv.index('-a') + 1]
+    if capital not in ['i', 'f']:
+        print("El tipo de capital debe ser 'i' (infinito) o 'f' (finito).")
+        sys.exit(1)
 
 
 def get_correct_arguments():
-    # Obtener el número de valores de los argumentos de la línea de comandos
-    num_valores = int(sys.argv[2])
-    corridas = int(sys.argv[4])
-    numero_elegido = int(sys.argv[6])
+    #num_valores = int(sys.argv[2])
+    #corridas = int(sys.argv[4])
+    #estrategia = sys.argv[sys.argv.index('-s') + 1]
+    #capital = sys.argv[sys.argv.index('-a') + 1]
 
-    return num_valores, corridas, numero_elegido
+    num_valores = 1000
+    corridas = 1
+    estrategia = 'm'
+    capital = 'i'
+
+    if '-e' in sys.argv:
+        eleccion = int(sys.argv[sys.argv.index('-e') + 1])
+    else:
+        eleccion = None
+
+    eleccion = 'rojo'
+    return num_valores, corridas, eleccion, estrategia, capital
 
 
 def generate_random_values(numeroDeTiradas):
-    # Generar los valores aleatorios entre 0 y 36 y almacenarlos en una lista
     valores = [random.randint(0, 36) for _ in range(numeroDeTiradas)]
     return valores
 
@@ -171,3 +218,95 @@ def calcular_varianza_calculada(numeroElegido, valoresAleatorios, numeroDeTirada
         varianzas_calculadas.append(varianza_calculada)
 
     return varianzas_calculadas
+
+
+def calcular_ganancia_perdida(eleccion, valor):
+    if eleccion == 'negro' and valor in NEGROS:
+        return 2
+    elif eleccion == 'rojo' and valor in ROJOS:
+        return 2
+    elif eleccion == 'c-1' and valor in COLUMNAS[1]:
+        return 3
+    elif eleccion == 'c-2' and valor in COLUMNAS[2]:
+        return 3
+    elif eleccion == 'c-3' and valor in COLUMNAS[3]:
+        return 3
+    elif eleccion in range(NUMEROS_RULETA) and eleccion == valor:
+        return 35
+    else:
+        return 0
+
+
+def martingala(apuesta_inicial, eleccion, valores, billetera):
+    apuesta = apuesta_inicial
+    historial = []
+    for valor in valores:
+        resultado = calcular_ganancia_perdida(eleccion, valor)
+        if resultado == 0:
+            billetera -= apuesta
+            apuesta *= 2  # Duplica la apuesta en caso de pérdida
+            historial.append(['P', billetera])
+        else:
+            billetera += apuesta * resultado
+            apuesta = apuesta_inicial  # Vuelve a la apuesta inicial en caso de ganancia
+            historial.append(['G', billetera])
+
+        if billetera < apuesta:
+            break
+
+    return billetera, historial
+
+
+def dalambert(apuesta_inicial, numero_elegido, valores):
+    capital = apuesta_inicial
+    ganancias = 0
+    perdidas = 0
+    apuesta = apuesta_inicial
+    for valor in valores:
+        resultado = calcular_ganancia_perdida(numero_elegido, valor)
+        if resultado == 0:
+            perdidas += 1
+            apuesta += 1  # Incrementa la apuesta en 1 en caso de pérdida
+        else:
+            ganancias += resultado
+            apuesta = max(apuesta - 1, apuesta_inicial)  # Reduce la apuesta en 1 en caso de ganancia, al menos hasta la apuesta inicial
+        capital -= apuesta  # Deduce la apuesta del capital
+    return ganancias, perdidas
+
+
+def fibonacci(apuesta_inicial, numero_elegido, valores):
+    capital = apuesta_inicial
+    ganancias = 0
+    perdidas = 0
+    apuesta_actual = apuesta_inicial
+    apuesta_anterior = 0
+    for valor in valores:
+        resultado = calcular_ganancia_perdida(numero_elegido, valor)
+        if resultado == 0:
+            perdidas += 1
+            apuesta_temporal = apuesta_actual
+            apuesta_actual += apuesta_anterior
+            apuesta_anterior = apuesta_temporal
+        else:
+            ganancias += resultado
+            apuesta_actual = apuesta_inicial
+            apuesta_anterior = 0
+        capital -= apuesta_actual
+    return ganancias, perdidas
+
+
+def jugar_ruleta(eleccion, estrategia, capital, valoresAleatorios):
+    apuesta_inicial = 1000
+    billetera = 10000
+    if estrategia == 'm':
+        billetera, resultados = martingala(apuesta_inicial, eleccion, valoresAleatorios, billetera)
+    elif estrategia == 'd':
+        ganancias, perdidas = dalambert(apuesta_inicial, eleccion, valoresAleatorios)
+    elif estrategia == 'f':
+        ganancias, perdidas = fibonacci(apuesta_inicial, eleccion, valoresAleatorios)
+    else:
+        print("Estrategia no válida.")
+        sys.exit(1)
+
+    print(f"Billetera final: {billetera}")
+    print(f"Historial: {resultados}")
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from FuncionesRuleta import *
 import numpy as np
 
-are_arguments_ok()
+#are_arguments_ok()
 parametros = get_correct_arguments()
 numeroDeTiradas = parametros[0]
 corridas = parametros[1]
 numeroElegido = parametros[2]
+estrategia = parametros[3]
+capital = parametros[4]
+
 valoresAleatorios = []
-print(f'Numero elegido:{numeroElegido}')
-print(f'Numero de tiradas:{numeroDeTiradas}')
-print(f'Numero de corridas:{corridas}')
+print(f'Numero de tiradas: {numeroDeTiradas}')
+print(f'Numero de corridas: {corridas}')
+print(f'Estrategia: {estrategia}')
+print(f'Tipo de capital: {capital}')
 
 for _ in range(corridas):
-  valoresAleatorios.append(generate_random_values(numeroDeTiradas))
-generate_all_plots(numeroDeTiradas,corridas,numeroElegido,valoresAleatorios)
+  valoresAleatorios.extend(generate_random_values(numeroDeTiradas))
+
+jugar_ruleta(numeroElegido, estrategia, capital, valoresAleatorios)
+
+# generate_all_plots(numeroDeTiradas, corridas, numeroElegido, valoresAleatorios)
